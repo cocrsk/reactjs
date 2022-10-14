c6a64c940ae6f768c7b03490f562cd8a6fe71459
rj103 <93474646+rj103@users.noreply.github.com>
2022-10-14 09:44:00 +0530

    Create reactjs.jsx

---
+++ reactjs.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Sans-Serif"
+  };
+  return (
+    <>
+      <h1 style={myStyle}>Hello Style!</h1>
+      <p>Add a little style!</p>
+    </>
+  );
+}
