e9d205fec082898196642c27d59289b9246e7741
rj103 <93474646+rj103@users.noreply.github.com>
2023-10-05 00:33:26 +0530

    Update added.jsx

--- added.jsx
+++ added.jsx
@@ -198,3 +198,255 @@ const MedicalForm = () => {
                 </label>
                 <Field
                   name="em_contact"
+                  type="number"
+                  placeholder="Enter Emergency No"
+                  className="form-control"
+                />
+                <ErrorMessage name="em_contact" className="form-text" />
+              </div>
+            </div>
+
+            {/* ========== Address Details ========== */}
+            <div className="heading">Adress Details</div>
+            <div className="grid-template-2">
+              {/* Address */}
+              <div className="address  margin-bottom">
+                <label htmlFor="address" className="form-label">
+                  Address
+                </label>
+                <Field
+                  name="address"
+                  id="address"
+                  type="text"
+                  placeholder="Enter Address"
+                  className="form-control"
+                />
+                <ErrorMessage name="address" className="form-text" />
+              </div>
+
+              {/* State */}
+              <div className="state  margin-bottom">
+                <label htmlFor="state" className="form-label">
+                  State
+                </label>
+                <Field
+                  name="state"
+                  id="state"
+                  as="select"
+                  className="form-select option-placeholder"
+                  onChange={(e) => {
+                    setState(e.target.value);
+                    this.handleChange(e);
+
+                  }}
+                >
+                  <option value="">Enter State</option>
+                  {states.map((item) => {
+                    return <option value={item.state}>{item.state}</option>;
+                  })}
+                </Field>
+              </div>
+
+              {/* City */}
+              <div className="city  margin-bottom">
+                <label htmlFor="city" className="form-label">
+                  City
+                </label>
+                <Field
+                  name="city"
+                  id="city"
+                  as="select"
+                  className="form-select option-placeholder"
+                >
+                  <option value="">Enter city/town/village</option>
+                  {states.map((item) => {
+                    return item.state === state
+                      ? item.districts.map((district) => {
+                          return <option value={district}>{district}</option>;
+                        })
+                      : null;
+                  })}
+                </Field>
+              </div>
+
+              {/* Country */}
+              <div className="country  margin-bottom">
+                <label htmlFor="country" className="form-label">
+                  Country
+                </label>
+                <Field
+                  name="country"
+                  id="country"
+                  as="select"
+                  className="form-select"
+                  disabled
+                >
+                  <option value="india">India</option>
+                </Field>
+              </div>
+
+              {/* Postal Code */}
+              <div className="pincode  margin-bottom">
+                <label htmlFor="pincode" className="form-label">
+                  Pincode
+                </label>
+                <Field
+                  name="pincode"
+                  id="pincode"
+                  type="number"
+                  placeholder="Enter Pincode"
+                  className="form-control"
+                />
+              </div>
+            </div>
+
+            {/* ========== Other Details ========== */}
+            <div className="heading">Other Details</div>
+
+            <div className="grid-template-3">
+              {/* Occupation */}
+              <div className="occupation  margin-bottom">
+                <label htmlFor="occupation" className="form-label">
+                  Occupation
+                </label>
+                <Field
+                  name="occupation"
+                  id="occupation"
+                  type="text"
+                  placeholder="Enter Occupation"
+                  className="form-control"
+                />
+                <ErrorMessage name="occupation" className="form-text" />
+              </div>
+
+              {/* Religion */}
+              <div className="religion  margin-bottom">
+                <label htmlFor="religion" className="form-label">
+                  Religion
+                </label>
+                <Field
+                  name="religion"
+                  id="religion"
+                  as="select"
+                  className="form-select option-placeholder"
+                >
+                  <option value="">Enter Religion</option>
+                  <option value="hinduism">Hinduism</option>
+                  <option value="islam">Islam</option>
+                  <option value="cristianism">Cristianism</option>
+                </Field>
+              </div>
+
+              {/* Marital Status */}
+              <div className="marital-status  margin-bottom">
+                <label htmlFor="marital_status" className="form-label">
+                  Marital Status
+                </label>
+                <Field
+                  name="marital_status"
+                  id="marital_status"
+                  as="select"
+                  className="form-select option-placeholder"
+                >
+                  <option value="">Enter Marital Status</option>
+                  <option value="Single">Single</option>
+                  <option value="Married">Married</option>
+                  <option value="Separated">Separated</option>
+                  <option value="Divorced">Divorced</option>
+                  <option value="Widowed">Widowed</option>
+                </Field>
+              </div>
+
+              {/* Blood Group */}
+              <div className="blood-group  margin-bottom">
+                <label htmlFor="blood_group" className="form-label">
+                  Blood Group
+                </label>
+                <Field
+                  name="blood_group"
+                  id="blood_group"
+                  as="select"
+                  className="form-select option-placeholder"
+                >
+                  <option value="">Group</option>
+                  <option value="A+">A+</option>
+                  <option value="A-">A-</option>
+                  <option value="B+">B+</option>
+                  <option value="B-">B-</option>
+                  <option value="O+">O+</option>
+                  <option value="O-">O-</option>
+                  <option value="AB+">AB+</option>
+                  <option value="AB-">AB-</option>
+                </Field>
+              </div>
+
+              {/* Nationality */}
+              <div className="nationality  margin-bottom">
+                <label htmlFor="nationality" className="form-label">
+                  Nationality
+                </label>
+                <Field
+                  name="nationality"
+                  id="nationality"
+                  as="select"
+                  className="form-select"
+                  disabled
+                >
+                  <option value="indian">Indian</option>
+                </Field>
+              </div>
+            </div>
+            <div className="btn-group">
+              <button className="btn-cancel" type="reset">
+                <span>Cancel</span>
+                <span id="btn-bottom">(ESC)</span>
+              </button>
+              <button type="submit" className="btn-submit">
+                <span>SUBMIT</span>
+                <span id="btn-bottom">
+                  (S <i className="bi bi-command"></i>)
+                </span>
+              </button>
+            </div>
+          </Form>
+        </Formik>
+        {/* ========== User List ========== */}
+        <table className="user-list">
+          <div className="user-table">User List</div>
+          <tbody>
+            <tr className="table-heading">
+              <th>Name</th>
+              <th>DoB/Age</th>
+              <th>Sex</th>
+              <th>Mobile</th>
+              <th>Email</th>
+              <th>Marital Status</th>
+              <th>Blood Group</th>
+              <th>Govt Id Type</th>
+              <th>Govt Id</th>
+            </tr>
+            {users.length
+              ? users.map((item) => {
+                  return (
+                    <tr className="table-data">
+                      <td>{item.name}</td>
+                      <td>{item.age}</td>
+                      <td>{item.sex}</td>
+                      <td>{item.mobile}</td>
+                      <td>{item.email}</td>
+                      <td>{item.marital_status}</td>
+                      <td>{item.blood_group}</td>
+                      <td>{item.id_type}</td>
+                      <td>{item.govt_id}</td>
+                    </tr>
+                  );
+                })
+              : null}
+          </tbody>
+        </table>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default MedicalForm;
