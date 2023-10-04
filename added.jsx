38a85a9f1fb2dfd4bf633d13763129514b06b485
rj103 <93474646+rj103@users.noreply.github.com>
2023-10-05 00:28:36 +0530

    Update added.jsx

--- added.jsx
+++ added.jsx
@@ -98,3 +98,103 @@ const MedicalForm = () => {
                 >
                   <option value="null">Enter Sex</option>
                   <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </Field>
+                <ErrorMessage name="sex" className="form-text" />
+              </div>
+            </div>
+            <div className="grid-template-2">
+              {/* Mobile */}
+              <div className="mobile  margin-bottom">
+                <label htmlFor="mobile" className="form-label">
+                  Mobile
+                </label>
+                <Field
+                  name="mobile"
+                  id="mobile"
+                  type="number"
+                  placeholder="Enter Mobile"
+                  className="form-control"
+                />
+                <ErrorMessage name="mobile" className="form-text" />
+              </div>
+              {/* Govt Issued Id */}
+              <div className="id-type  margin-bottom">
+                <label htmlFor="id_type" className="form-label">
+                  Govt Issued ID
+                </label>
+                <Field
+                  name="id_type"
+                  id="id_type"
+                  as="select"
+                  className="form-select option-placeholder"
+                >
+                  <option value="">ID Type</option>
+                  <option value="AAdhar Card">Aadhar Card</option>
+                  <option value="Voter Card">Voter ID Card</option>
+                  <option value="Ration Card">Ration Card</option>
+                </Field>
+              </div>
+              <div className="govt-id  margin-bottom">
+                <Field
+                  name="govt_id"
+                  type="text"
+                  placeholder="Enter Govt ID"
+                  className="form-control"
+                />
+                <ErrorMessage name="govt_id" className="form-text" />
+              </div>
+            </div>
+
+            {/* ========== Contact Details ========== */}
+            <div className="heading">Contact Details</div>
+            <div className="grid-template-2">
+              {/* Guardian Details */}
+              <div className="guardian  margin-bottom">
+                <label htmlFor="guardian" className="form-label">
+                  Guardian Details
+                </label>
+                <Field
+                  name="guardian"
+                  id="guardian"
+                  as="select"
+                  className="form-select option-placeholder"
+                >
+                  <option value="">Select Guardian</option>
+                  <option value="father">Father</option>
+                  <option value="mother">Mother</option>
+                  <option value="legal_guardian">Legal Guardian</option>
+                </Field>
+                <Field
+                  name="guardian_name"
+                  id="guardian_name"
+                  type="text"
+                  placeholder="Enter Guardian Name"
+                  className="form-control"
+                />
+                <ErrorMessage name="guardian_name" className="form-text" />
+              </div>
+
+              {/* Email */}
+              <div className="email  margin-bottom">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <Field
+                  name="email"
+                  id="email"
+                  type="email"
+                  placeholder="Enter Email"
+                  className="form-control"
+                />
+                <ErrorMessage name="email" className="form-text" />
+              </div>
+
+              {/* Emergency Contact Number */}
+              <div className="em-contact  margin-bottom">
+                <label htmlFor="em_contact" className="long form-label">
+                  Emergency Contact No
+                </label>
+                <Field
+                  name="em_contact"
